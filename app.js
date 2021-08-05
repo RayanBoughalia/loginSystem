f9382c4f1e973827caf36a6d6637a33794e6e082
_0M3G4_ <58949193+RayanBoughalia@users.noreply.github.com>
2021-08-05 17:46:48 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+var fs = require('fs');
+
+fs.readFile('./login.txt', 'utf8', (err, data) => {
+	if (err) throw err;
+	let username = data.split(' ')[1].split('\r')[0];
+	let password = data.split(' ')[2].split('\r')[0];
+	if (username == 'user' && password == 'password') {
+		console.log('Success !')
+	}else{
+		console.log('Game Over !');
+	}
+});
